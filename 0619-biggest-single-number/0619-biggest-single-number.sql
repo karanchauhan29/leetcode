b07c54ad653261c7aa26376bf8c0ee07cbba42bf
karan chauhan <karan.chauhan_cs23@gla.ac.in>
2026-09-13 13:46:20 +0530

    Time: 475 ms (54.19%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select MAX(num) as num
+from MyNumbers
+where num in(select num 
+            from MyNumbers 
+            group by num 
+            having count(*)=1);
